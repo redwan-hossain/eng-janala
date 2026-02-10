37241e63a203b7d601da78aceb3303ec5cbdd975
redwan-hossain <rfredwan0@gmail.com>
2026-02-10 11:15:17 +0600

    lesson word dynamic modal added

--- script/index.js
+++ script/index.js
@@ -7,6 +7,39 @@ const loadLesson = () => {
 }
 
 
+const loadLevelWord = (id) => {
+    const url = (`https://openapi.programming-hero.com/api/level/${id}`)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            displayLevelWord(data.data);
+        })
+}
+
+const displayLevelWord = (words) => {
+
+    const wordContainer = document.querySelector("#word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        wordContainer.innerHTML += `
+    <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h1 class="font-bold text-2xl">${word.word}</h1>
+            <p class="font-semibold">meaning/pronounce</p>
+            <div class="bangla-fontt text-2xl font-medium">${word.meaning}/>${word.pronunciation}</div>
+
+            <div class="flex justify-between items-center">
+                <button class="btn"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn"><i class="fa-solid fa-volume-low"></i></button>
+            </div>
+
+        </div>
+
+       `
+
+    })
+}
+
 const displayLesson = (lessons) => {
 
     const lesson_container = document.querySelector("#lesson-container");
@@ -16,12 +49,12 @@ const displayLesson = (lessons) => {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
         
-                              <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson- ${lesson.level_no}
+                              <button onclick ="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson- ${lesson.level_no}
                                 </button>
 
         `
         lesson_container.appendChild(btnDiv);
-        
+
     });
 }
 
